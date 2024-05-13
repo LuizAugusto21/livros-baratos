64eb19aaddb3c0fbc9d27dffbf8779a7966c40d2
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-13 15:07:03 -0300

    Criação do componente button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./button.scss";
+
+export default function Button(){
+    return(
+        <div>
+            <button className="botao-voltar">Voltar</button>
+        </div>
+    );
+}
