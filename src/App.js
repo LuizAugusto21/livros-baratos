9e2b015c7eb302cc4087d71e846475d08f7fc2dd
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-13 15:07:45 -0300

    Alteração do app para testar a wishlist page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header'
 import SearchBar from './components/SearchBar/SearchBar';
 import './App.css';
+import Wishlist from './pages/wishlist/Wishlist';
 
 function App() {
   return (
     <div className="App">
-      <Header isLogged={true} isHome={false}></Header>
+      <Wishlist />
+      {/* <Header isLogged={true} isHome={false}></Header> */}
     </div>
   );
 }
