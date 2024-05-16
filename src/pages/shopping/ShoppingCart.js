201cf9a8b1eca0d4614bafcec6589648d3f05564
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-16 15:12:25 -0300

    Implementação da tela de carrinho vazio

--- src/pages/shopping/ShoppingCart.js
+++ src/pages/shopping/ShoppingCart.js
@@ -1,7 +1,7 @@
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import "./ShoppingCart.css";
-
+import Icone from "../../images/cil_cart.png";
 
 // if cart.empty() ? page with list : page with image 
 // !!! Utilizar a logica da minha todo list do github !!!
@@ -11,12 +11,8 @@ export default function ShoppingCart(){
         <div>
             <Header isLogged={true} isHome={true}></Header>
             <div className="container-principal">
-                <div className="lista-produtos">
-                    {/* TODO */}
-                </div>
-                <div className="produtos-no-carrinho">
-                    {/* TODO */}
-                </div>
+                <img className="carrinho-de-compras" width={200} src={Icone}/>
+                <h1 className="texto-carrinho-vazio">O carrinho está vazio</h1>
             </div>
             
             <div className="container-botao-voltar">
