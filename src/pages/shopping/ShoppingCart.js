8d92faf55d8b2b38c227f7c0b8d40fb6e8139247
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-20 21:31:42 -0300

    Adição de classes e id nas tags img e span. Criação da assinatura da função de remoção

--- src/pages/shopping/ShoppingCart.js
+++ src/pages/shopping/ShoppingCart.js
@@ -1,11 +1,19 @@
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import "./ShoppingCart.css";
-import Icone from "../../images/cil_cart.png";
+import shoppingCartIcon from "../../images/cil_cart.png";
+import trashIcon from "../../images/trash-icon.png";
 
 
 // Implementação provisória do estado do carrinho 
 let carrinhoVazio = false;
+
+// TODO: Será a função de remover o item do carrinho
+function removerItem(){
+    // TODO: Implementar a lógica de deleção
+   document.getElementById("trash-icon").style.backgroundColor = "red";
+
+}
 
 export default function ShoppingCart(){
     return(  
@@ -16,16 +24,21 @@ export default function ShoppingCart(){
                         carrinhoVazio
                         ?
                         <>
-                            <img className="carrinho-de-compras" width={200} src={Icone} /><h1 className="texto-carrinho-vazio"> O carrinho está vazio</h1></>
+                            <img className="carrinho-de-compras" width={200} src={shoppingCartIcon} /><h1 className="texto-carrinho-vazio"> O carrinho está vazio</h1></>
                         :
                         <>
                             <div className="bloco-lista-itens">
                                 <h2> Carrinho de compras </h2>
                                 <div className="lista-itens">
+                                    <div className="preco-e-remover">
+                                        <span className="preco-text">R$ 15,00</span>
+                                        <img 
+                                        id="trash-icon" className="trash-icon" src={trashIcon} onClick={ () => {removerItem()} }/>
+                                    </div>
                                 </div>
                             </div>
                             <div className="resumo-pedido">
-                            <h2> Div vermelha </h2>
+                            <h2> Resumo do pedido </h2>
                             </div>
                         </>
                     }
