8211bb5598fa5a4ee0d417e6db3252a51b690246
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-21 22:28:51 -0300

    Movendo a div 'item-carrinho' para transformar em componente

--- src/pages/shopping/ShoppingCart.js
+++ src/pages/shopping/ShoppingCart.js
@@ -2,19 +2,11 @@ import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import "./ShoppingCart.css";
 import shoppingCartIcon from "../../images/cil_cart.png";
-import trashIcon from "../../images/trash-icon.png";
-import bookCover from "../../images/Harry-Potter-1.jpg"
+import ShoppingCartItem from "../../components/ShoppingCartItem/ShoppingCartItem";
 
 
 // Implementação provisória do estado do carrinho 
 let carrinhoVazio = false;
-
-// TODO: Será a função de remover o item do carrinho
-function removerItem(){
-    // TODO: Implementar a lógica de deleção
-   document.getElementById("trash-icon").style.backgroundColor = "red";
-
-}
 
 export default function ShoppingCart(){
     return(  
@@ -30,20 +22,7 @@ export default function ShoppingCart(){
                         <>
                             <div className="bloco-lista-itens">
                                 <h2> Carrinho de compras </h2>
-                                <div className="lista-itens">
-                                    <div className="capa-livro">
-                                        <img className = "capa-livro-image" src={bookCover} width={70} height={85}/>
-                                    </div>
-                                    <div className="titulo-livro"> Harry Potter and The Philosofer Stone
-                                    <div className="autor-livro"> J.K. Rowling </div>
-                                    </div>
-                                    
-                                    <div className="preco-e-remover">
-                                        <span className="preco-text">R$ 15,00</span>
-                                        <img 
-                                        id="trash-icon" className="trash-icon" src={trashIcon} onClick={ () => {removerItem()} }/>
-                                    </div>
-                                </div>
+                                <ShoppingCartItem/>
                             </div>
                             <div className="resumo-pedido">
                             <h2> Resumo do pedido </h2>
