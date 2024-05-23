a58913d4bbd55b31c938f6721a9275ca049ed972
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-23 20:29:10 -0300

    Adição de props ao componente

--- src/components/ShoppingCartItem/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem/ShoppingCartItem.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import "./ShoppingCartItem.scss";
 import trashIcon from "../../images/trash-icon.png";
-import bookCover from "../../images/Harry-Potter-1.jpg"
+import bookCover from "../../images/Harry-Potter-2.jpg"
 
 // TODO: Será a função de remover o item do carrinho
 function removerItem(){
     // TODO: Implementar a lógica de deleção
-   document.getElementById("trash-icon").style.backgroundColor = "red";
 
+    // document.getElementById("trash-icon").style.backgroundColor = "red";
 }
 
-export default function ShoppingCartItem(){
+export default function ShoppingCartItem({book_cover=bookCover, title, author, price}){
     return(
         <div>
             <div className="item-carrinho">
                 <div className="capa-livro">
-                    <img className = "capa-livro-image" src={bookCover} width={70} height={85}/>
+                    <img className = "capa-livro-image" src={book_cover} width={70} height={85}/>
                 </div>
-                <div className="titulo-livro"> Harry Potter and The Philosofer Stone
-                <div className="autor-livro"> J.K. Rowling </div>
+                <div className="titulo-livro">{title}
+                <div className="autor-livro">{author}</div>
                 </div>
                 
                 <div className="preco-e-remover">
-                    <span className="preco-text">R$ 15,00</span>
+                    <span className="preco-text">R$ {price}</span>
                     <img 
                     id="trash-icon" className="trash-icon" src={trashIcon} onClick={ () => {removerItem()} } />
                 </div>
