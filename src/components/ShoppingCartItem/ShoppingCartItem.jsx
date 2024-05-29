3705ad22a2cf335967037147b27abba422b431ab
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-28 21:52:28 -0300

    Sem mudanças significativas

--- src/components/ShoppingCartItem/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem/ShoppingCartItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ShoppingCartItem.scss";
 import trashIcon from "../../images/trash-icon.png";
-import bookCover from "../../images/Harry-Potter-2.jpg"
+import bookCover from "../../images/Harry-Potter-1.jpg"
 
 // TODO: Será a função de remover o item do carrinho
 function removerItem(){
@@ -15,7 +15,7 @@ export default function ShoppingCartItem({book_cover=bookCover, title, author, p
         <div>
             <div className="item-carrinho">
                 <div className="capa-livro">
-                    <img className = "capa-livro-image" src={book_cover} width={70} height={85}/>
+                    <img className = "capa-livro-image" alt="capa do livro" src={book_cover} width={70} height={85}/>
                 </div>
                 <div className="titulo-livro">{title}
                 <div className="autor-livro">{author}</div>
@@ -24,7 +24,7 @@ export default function ShoppingCartItem({book_cover=bookCover, title, author, p
                 <div className="preco-e-remover">
                     <span className="preco-text">R$ {price}</span>
                     <img 
-                    id="trash-icon" className="trash-icon" src={trashIcon} onClick={ () => {removerItem()} } />
+                    id="trash-icon" className="trash-icon" alt="icone de lixeira" src={trashIcon} onClick={ () => {removerItem()} } />
                 </div>
             </div>
         </div>
