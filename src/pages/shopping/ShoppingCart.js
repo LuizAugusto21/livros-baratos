7ae7547c19a01223bb230997beb6b135bdd4a2ee
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-29 17:06:51 -0300

    Modelando o quadro de resumo de pedidos

--- src/pages/shopping/ShoppingCart.js
+++ src/pages/shopping/ShoppingCart.js
@@ -24,13 +24,24 @@ export default function ShoppingCart(){
                                 <h2> Carrinho de compras </h2>
                                 {/* TODO: Implementar a lógica de preenchimento dos dados do livro no componente */}
                                 <ShoppingCartItem title="Harry Potter and the Philosofer Stone" author="J.K. Rowling" price={17}/> 
+                                <ShoppingCartItem title="Harry Potter and the Philosofer Stone" author="J.K. Rowling" price={17}/> 
                             </div>
                             <div className="resumo-pedido">
-                            
-                            {/* Prosseguir a implementação amanhã */}
-                            <div></div>
-                            <h2> Resumo do pedido </h2>
-                            <h2>Total</h2>
+                                <h2> Resumo do pedido </h2>
+                                <div className="item-resumo-pedido">
+                                    <div className="item-resumo-texto">Item 1</div>
+                                    <div className="item-resumo-preco">R$ 15,00</div>
+                                </div>
+                                <div className="item-resumo-pedido">
+                                    <div className="item-resumo-texto">Item 1</div>
+                                    <div className="item-resumo-preco">R$ 15,00</div>
+                                </div>
+                                <hr></hr>
+                                <div className="total-resumo-pedido">
+                                    <div className="total-resumo-texto">Total</div>
+                                    <div className="total-resumo-preco">R$ 30,00</div>
+                                </div>
+                                
                             </div>
                         </>
                     }
