72d2a552cba978826ed82a96609dd6671dc20645
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-28 21:52:43 -0300

    Sem mudanças significativas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import ShoppingCart from "./pages/shopping/ShoppingCart";
-import Wishlist from "./pages/wishlist/Wishlist";
+// import Wishlist from "./pages/wishlist/Wishlist";
+
 
 function App() {
   return (
     <div className="App">
+      <ShoppingCart/>
       {/* <Wishlist/> */}
-      <ShoppingCart />
     </div>
   );
 }
