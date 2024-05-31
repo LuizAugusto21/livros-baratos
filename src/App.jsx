d504d6bf1657caa42fd87a1615960672be69212e
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-30 21:22:46 -0300

    Alteração para poder testar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./pages/login/Login";
-import Cadastro from "./pages/singup/Cadastro";
-import homepage from "./pages/homepage/homepage";
+import Wishlist from "./pages/wishlist/Wishlist";
+// import Homepage from "./pages/homepage/homepage";
 
 function App(props) {
   return (
-    <Router>
       <div className="app">
-        <div className="content">
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/cadastro" component={Cadastro} />
-            <Route path="/" component={homepage} />
-          </Switch>
+          <Wishlist />
         </div>
-      </div>
-    </Router>
   );
 }
 
