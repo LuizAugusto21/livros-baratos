05e65c799af93d4e514649f662d700b27b0232f8
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-30 21:23:54 -0300

    Criando a lógica de construção do componente de livro favoritado

--- src/pages/wishlist/Wishlist.js
+++ src/pages/wishlist/Wishlist.js
@@ -1,18 +1,70 @@
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "../../components/Header/Header";
+import BookCard from "../../components/BookCard/BookCard";
 import Button from "../../components/Button/Button";
+
 import "./Wishlist.css";
 
 
-export default function wishlist(){
+
+export default function Wishlist(){
+
+    const [data, setData] = useState([]);
+    const [startIndex, setStartIndex] = useState(0);
+    const itemsPerLine = 5;
+
+    useEffect(() => {
+        fetch("http://localhost:3000/data/BookData.json")
+          .then((response) => response.json())
+          .then((data) => {
+            setData(data);
+          })
+          .catch((error) => {
+            console.error("Erro ao buscar os dados:", error);
+          });
+      }, []);
+
+      // AINDA BOLANDO A LÓGICA QUE VAI SER APLICADA NA CONSTRUÇÃO
+
+      // PONTO IMPORTANTES: CADA LINHA TEM 5 FAVORITOS, ENTÃO AO CHEGAR AO QUINTO ELE TEM QUE PULAR PARA A PRÓXIMA DIV
+      // PRECISO DE UM JSON PARA GUARDAR OS FAVORITOS (TEM QUE SER JSON?)
+
+      const listaStorage = localStorage.getItem("ListaFavoritos");
+      const [favoritesList, setFavoritesList] = useState(listaStorage ? JSON.parse(listaStorage) : []);
+
+      useEffect(() => {
+        localStorage.setItem("ListaFavoritos", JSON.stringify(favoritesList))
+      }, [favoritesList])
+
+
     return(
         <div>
             <Header isLogged={true} isHome={true}></Header>
-            <h1 className="titulo-favorito">Wishlist</h1>
-            <div className="conteiner-botao"> 
-                <h1>aaaaaaaa</h1>
-                <Button></Button>
+            <div className="container-principal">
+                <h1 className="titulo-favorito">Wishlist</h1>
+                <div className="lista-favoritos">
+                    <>
+                      {
+                        data
+                          .slice(startIndex, itemsPerLine)
+                          .map((item, index) => {
+                            const { name, author } = item;
+                            return (
+                              <BookCard 
+                                key={index}
+                                preco={15}
+                                nomeLivro={name}
+                                Autor={author}
+                              />
+                            );    
+                        }
+                    )}
+                    </>
+                </div>
+                <div className="container-botao-voltar"> 
+                    {/* <Button></Button> */}
+                </div>
             </div>
         </div>
     );
