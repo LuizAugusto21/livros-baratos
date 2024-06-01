13e1a274c9e28fa41b102852c29c197ab17ef421
Pedro Henrique <pedrosales2806@gmail.com>
2024-06-01 12:06:40 -0300

    Mudanças para testes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Wishlist from "./pages/wishlist/Wishlist";
+// import ShoppingCart from "./pages/shopping/ShoppingCart";
 // import Homepage from "./pages/homepage/homepage";
 
 function App(props) {
   return (
       <div className="app">
           <Wishlist />
+          {/* <ShoppingCart></ShoppingCart> */}
         </div>
   );
 }
