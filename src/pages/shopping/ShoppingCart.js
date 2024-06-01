4ea29885971f806a3c77ce6996ecd2e7c29de1e6
Pedro Henrique <pedrosales2806@gmail.com>
2024-06-01 20:59:29 -0300

    Refatoração do código para construir o componente de itens no carrinho

--- src/pages/shopping/ShoppingCart.js
+++ src/pages/shopping/ShoppingCart.js
@@ -1,3 +1,5 @@
+import React, {useState, useEffect} from "react";
+
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import "./ShoppingCart.css";
@@ -9,53 +11,96 @@ import ShoppingCartItem from "../../components/ShoppingCartItem/ShoppingCartItem
 let carrinhoVazio = false;
 
 export default function ShoppingCart(){
+    const [itensOnCart, setItensOnCart] = useState([]);
+    const [count, setCount] = useState(0);
+
+    // Preenche a lista de itens no carrinho
+    useEffect(() => {
+        fetch("http://localhost:3000/data/ItensOnCartData.json")
+        .then((response) => response.json())
+        .then((itens) => {
+            setItensOnCart(itens);
+        })
+        .catch((error) => {
+            console.error("Erro ao buscar os dados:", error);
+        });
+    }, []);
+
+    function emptyCart(){
+        if(itensOnCart.length <= 0) return true
+        else return false;
+    }
+
     return(  
         <div>
             <Header isLogged={true} isHome={true}></Header>
-                    {
-                        carrinhoVazio
-                        ?
+            {
+                emptyCart()
+                ?
+                <>
+                    <div className="container-principal-vazio">
+                        <img className="carrinho-de-compras" width={200} src={shoppingCartIcon} alt="Carrinho de compras vazio"/>
+                        <h1 className="texto-carrinho-vazio"> O carrinho está vazio</h1>
+                            <Button/>
+                    </div>
+                </>
+                :
+                <div className="container-principal">
+                    <div className="bloco-lista-itens">
+                        <h2> Carrinho de compras </h2>
                         <>
-                            <div className="container-principal-vazio">
-                                <img className="carrinho-de-compras" width={200} src={shoppingCartIcon} alt="Carrinho de compras vazio"/>
-                                <h1 className="texto-carrinho-vazio"> O carrinho está vazio</h1>
-                                <Button></Button>
-                            </div>
+                            {
+                                itensOnCart
+                                    .slice()
+                                    .map((item, index) => {
+                                    const {name, author} = item;
+                                    let count=1;
+                                    return(
+                                        <ShoppingCartItem 
+                                            key={index}
+                                            name={name}
+                                            author={author}
+                                            price={20} />
+                                    )
+                                    }
+                                )
+                            }
                         </>
-                        :
+                    </div>
+                    <div className="resumo-pedido">
+                        <h2> Resumo do pedido </h2>
+                        <div className="item-resumo-pedido">
                         <>
-                            <div className="container-principal">
-                                <div className="bloco-lista-itens">
-                                    <h2> Carrinho de compras </h2>
-                                    {/* TODO: Implementar a lógica de preenchimento dos dados do livro no componente */}
-                                    <ShoppingCartItem title="Harry Potter and the Philosofer Stone" author="J.K. Rowling" price={17}/> 
-                                    <ShoppingCartItem title="Harry Potter and the Philosofer Stone" author="J.K. Rowling" price={17}/> 
-                                </div>
-                                <div className="resumo-pedido">
-                                    <h2> Resumo do pedido </h2>
-                                    <div className="item-resumo-pedido">
-                                        <div className="item-resumo-texto">Item 1</div>
-                                        <div className="item-resumo-preco">R$ 15,00</div>
-                                    </div>
-                                    <div className="item-resumo-pedido">
-                                        <div className="item-resumo-texto">Item 1</div>
-                                        <div className="item-resumo-preco">R$ 15,00</div>
-                                    </div>
-                                    <hr></hr>
-                                    <div className="total-resumo-pedido">
-                                        <div className="total-resumo-texto">Total</div>
-                                        <div className="total-resumo-preco">R$ 30,00</div>
-                                    </div>                   
-                                </div>
-                                <div className="container-botao-geral">
-                                        <Button></Button>
-                                    </div>
-                                    <div className="container-botao-geral">
-                                        <Button text="Comprar"></Button>
-                                    </div>
-                            </div>
+                            {
+                                itensOnCart
+                                    .slice()
+                                    .map(() => {
+                                    return(
+                                        <>
+                                            <div className="item-resumo-texto">Item 1</div>
+                                            <div className="item-resumo-preco">R$ 20</div>
+                                            {/* TALVEZ EU TENHA QUE CRIAR UM COMPONENTE */}
+                                        </>
+                                    )
+                                    }
+                                )
+                            }
                         </>
-                    }
+                        </div>
+                        <hr></hr>
+                        <div className="total-resumo-pedido">
+                            <div className="total-resumo-texto">Total</div>
+                            <div className="total-resumo-preco">R$ 30,00</div>
+                        </div> 
+                        <div className="container-botao-geral">
+                            <Button/>
+                        </div>
+                        <div className="container-botao-geral">
+                            <Button text="Comprar"/>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
