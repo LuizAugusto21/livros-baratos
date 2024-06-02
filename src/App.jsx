865f5f25710d6d96a13c09fa7d9fbe9d2eea2788
Pedro Henrique <pedrosales2806@gmail.com>
2024-06-02 14:45:27 -0300

    Mudanças sem importancia

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Wishlist from "./pages/wishlist/Wishlist";
-// import ShoppingCart from "./pages/shopping/ShoppingCart";
+// import Wishlist from "./pages/wishlist/Wishlist";
+import ShoppingCart from "./pages/shopping/ShoppingCart";
 // import Homepage from "./pages/homepage/homepage";
 
 function App(props) {
   return (
       <div className="app">
-          <Wishlist />
-          {/* <ShoppingCart></ShoppingCart> */}
+          {/* <Wishlist /> */}
+          <ShoppingCart></ShoppingCart>
         </div>
   );
 }
