cc20c0994d682df757c68aa313f82045be1254ee
Pedro Henrique <pedrosales2806@gmail.com>
2024-06-03 17:52:12 -0300

    Commit para mudar de branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-// import Wishlist from "./pages/wishlist/Wishlist";
-import ShoppingCart from "./pages/shopping/ShoppingCart";
-// import Homepage from "./pages/homepage/homepage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App(props) {
+import Header from './components/Header/Header'
+import  Footer from './components/Footer/Footer'
+
+import Login from "./pages/login/Login";
+import Signup from "./pages/singup/Cadastro";
+import Homepage from "./pages/homepage/homepage";
+import Wishlist from './pages/wishlist/Wishlist'
+import ShoppingCart from './pages/shopping/ShoppingCart'
+import "./global.scss";
+
+function App() {
   return (
-      <div className="app">
-          {/* <Wishlist /> */}
-          <ShoppingCart></ShoppingCart>
+    <Router>
+       <div className="app-container">
+          <Header isHome={true}/>
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Signup />} />
+              <Route path="/wishlist" element={<Wishlist />} />
+              <Route path="/carrinho" element={<ShoppingCart />} />
+              {/* Adicione mais rotas conforme necessário */}
+            </Routes>
+          </main>
+          <Footer />
         </div>
+    </Router>
   );
 }
 
