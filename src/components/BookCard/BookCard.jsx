1048caa038c3928a066bdd2adb7900326f61a56b
Pedro Henrique <pedrosales2806@gmail.com>
2024-06-05 22:00:38 -0300

    Criação da navegabilidade para a nova página (detalhes do livro)

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./BookCard.scss";
+import { Link, useNavigate } from "react-router-dom";
+
+
 
 export default function BookCard( {preco, nomeLivro, Autor}) {
+  
+  const navigate = useNavigate();
+  
   return (
     <div className="BookCard-box">
       <div className="BookCard-content">
-        <img src="/default-placeholder.png" alt="" height={166} />
+        <img src="/default-placeholder.png" alt="" height={166} onClick={() => navigate("/detalhes")}/>
         <div className="BookCard-info">
           <p>
             <span>
