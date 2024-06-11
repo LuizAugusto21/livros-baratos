a65f14d096a868f82b97d2119c7972768db01f98
Pedro Henrique <pedrosales2806@gmail.com>
2024-06-11 20:06:07 -0300

    Alterações para possibilitar a 'navegabilidade'

--- src/components/BookCardAlternative/BookCardAlternative.jsx
+++ src/components/BookCardAlternative/BookCardAlternative.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./BookCardAlternative.module.scss";
-import { Link, useNavigate } from "react-router-dom";
 import bookCover from "../../images/default-placeholder.png";
 
 
-export default function BookCardAlternative( {preco, nomeLivro, Autor}) {
-  
-  const navigate = useNavigate();
+export default function BookCardAlternative( {nomeLivro, Autor, preco, descricao, ano, generos, favoritado}) {
+
+    const [ book, setBook ] = useState(null);
+
+    useEffect(()=> {
+        // Cria um objeto com as informações do livro atual
+        const newBook = { name: nomeLivro,
+          author: Autor,
+          price: preco,
+          genres: generos,
+          description: descricao,
+          year: ano,
+          isFavorited: favoritado};
+    
+          setBook(newBook);
+      }, [])
+    
+      const handleDetailClick = () => {
+        
+        const jsonValue = JSON.stringify(book);
+        localStorage.setItem("currentBook", jsonValue);
+    
+        // Força a recarga da página
+        window.location.reload();
+    
+      }
   
   return (
     <div className={styles["BookCardAlternative-box"]}>
         <div className={styles["BookCardAlternative-content"]}>
-            <img src={bookCover} alt="capa do livro" height={140} width={97.5} onClick={() => navigate("/detalhes")} />
+            <img src={bookCover} alt="capa do livro" height={140} width={97.5} onClick={ handleDetailClick } />
             <div className={styles["BookCardAlternative-info"]}>
                 <p>
                     <div className={styles["price-condition"]}>
