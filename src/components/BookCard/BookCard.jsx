6df8ebc481df13bd281a4286c99cf0a08aef99aa
Pedro Henrique <pedrosales2806@gmail.com>
2024-06-11 20:08:17 -0300

    Ajuste de código

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from "react";
 import styles from "./BookCard.module.scss";
-import { Link, useNavigate } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 
 export default function BookCard( {nomeLivro, Autor, preco, descricao, ano, generos, favoritado}) {
